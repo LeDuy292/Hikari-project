e2ec5fd696477f5be96e83fdf6742fc9a2419c10
Tran Dinh Quy <trandinhquy1411@gmail.com>
2025-06-17 23:55:44 +0700

    Add course management servlets and DAO implementation

---
+++ src/main/java/model/Course.java
@@ -0,0 +1,125 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.util.Date;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Course {
+    private int courseNum;
+    private String courseID;
+    private String title;
+    private String description;
+    private double fee;
+    private int duration;
+    private Date startDate;
+    private Date endDate;
+    private boolean isActive;
+    private int classCount;
+
+    public Course() {
+    }
+
+    public Course(int courseNum, String courseID, String title, String description, double fee, int duration, Date startDate, Date endDate, boolean isActive) {
+        this.courseNum = courseNum;
+        this.courseID = courseID;
+        this.title = title;
+        this.description = description;
+        this.fee = fee;
+        this.duration = duration;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.isActive = isActive;
+    }
+
+    public int getCourseNum() {
+        return courseNum;
+    }
+
+    public void setCourseNum(int courseNum) {
+        this.courseNum = courseNum;
+    }
+
+    public String getCourseID() {
+        return courseID;
+    }
+
+    public void setCourseID(String courseID) {
+        this.courseID = courseID;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getFee() {
+        return fee;
+    }
+
+    public void setFee(double fee) {
+        this.fee = fee;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public boolean isIsActive() {
+        return isActive;
+    }
+
+    public void setIsActive(boolean isActive) {
+        this.isActive = isActive;
+    }
+
+    public int getClassCount() {
+        return classCount;
+    }
+
+    public void setClassCount(int classCount) {
+        this.classCount = classCount;
+    }
+    
+    
+    @Override
+    public String toString() {
+        return "Course{" + "courseNum=" + courseNum + ", courseID=" + courseID + ", title=" + title + ", description=" + description + ", fee=" + fee + ", duration=" + duration + ", startDate=" + startDate + ", endDate=" + endDate + ", isActive=" + isActive + '}';
+    }
+}
